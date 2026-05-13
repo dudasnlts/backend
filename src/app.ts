dfdc448b8dacb9ba969bd5b99e484bf033d10471
dudasnlts <marriia.eduarda09@gmail.com>
2026-05-13 17:11:29 -0300

    acabar ivy

--- src/app.ts
+++ src/app.ts
@@ -8,6 +8,11 @@ import userRoutes from "./routes/userRoutes";
 import authRoutes from "./routes/authRoutes";
 import cartRoutes from "./routes/cartRoutes";
 import orderRoutes from "./routes/orderRoutes";
+import joiasRoutes from "./routes/joias.routes";
+import perfumeRoutes from "./routes/perfume.routes";
+import pingentesRoutes from "./routes/pingentes.routes";
+import relogioRoutes from "./routes/relogio.routes";
+import saleRoutes from "./routes/sale.routes";
 
 const app = express();
 
@@ -16,7 +21,6 @@ app.use(cors());
 app.use(express.json());
 
 // LIBERAR IMAGENS DA PASTA UPLOADS
-
 app.use(
   "/uploads",
   express.static(
@@ -25,17 +29,16 @@ app.use(
 );
 
 // ROUTES
-
 app.use("/api/categories", categoryRoute);
-
 app.use("/api/products", productsRoute);
-
 app.use("/api/users", userRoutes);
-
 app.use("/api/auth", authRoutes);
-
 app.use("/api/cart", cartRoutes);
-
 app.use("/api/orders", orderRoutes);
+app.use("/api/joias", joiasRoutes);
+app.use("/api/perfumes", perfumeRoutes);
+app.use("/api/pingentes", pingentesRoutes);
+app.use("/api/relogios", relogioRoutes);
+app.use("/api/sales", saleRoutes);
 
 export default app;
